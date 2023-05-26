140f261e038f258c0469f3deb338b39a9671f2ca
EmiMinetti <eminetti99@gmail.com>
2023-05-25 21:58:44 -0300

    Complete props

--- src/Components/Pokemon full/Component Full/BaseStats.jsx
+++ src/Components/Pokemon full/Component Full/BaseStats.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 
-export const BaseStats = () => {
+export const BaseStats = (props) => {
   return <div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+    <div className='divStat'>
+      <p>HP</p>
+      <p>{props.hp}</p>
+    </div>
+    <div className='divStat'>
+      <p>ATK</p>
+      <p>{props.atk}</p>
+    </div>
+    <div className='divStat'>
+      <p>DEF</p>
+      <p>{props.def}</p>
+    </div>
+    <div className='divStat'>
+      <p>SATK</p>
+      <p>{props.satk}</p>
+    </div>
+    <div className='divStat'>
+      <p>SDEF</p>
+      <p>{props.sdef}</p>
+    </div>
+    <div className='divStat'>
+      <p>SPD</p>
+      <p>{props.spd}</p>
+    </div>
   </div>
 }
