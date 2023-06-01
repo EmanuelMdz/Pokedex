c657efbefc204b4e8ab25dec2a754b415237bcf6
Emanuel Mendoza Mori <130607153+EmanuelMdz@users.noreply.github.com>
2023-06-01 20:15:06 -0300

    Csscasifinal

--- src/Components/Extras/PokemonPreview.jsx
+++ src/Components/Extras/PokemonPreview.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import "./PokemonPreview.css";
 
 export const PokemonPreview = (props) => {
+  const capitalizeName = (name) => {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  };
   return (
     <div className={`container_preview ${props.type}`}>
-      <p>#{props.number}</p>
+      <p className={`container_p ${props.type}`}>#{props.number}</p>
       <div className="imageContent">
         {" "}
         <img src={props.img} alt="" />
       </div>
 
-      <p className="name">{props.name}</p>
+      <p className="name">{capitalizeName(props.name)}</p>
     </div>
   );
 };
